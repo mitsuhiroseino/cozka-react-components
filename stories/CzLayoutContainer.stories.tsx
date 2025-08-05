aad54c50ffd4b3c43c364a771459d4eddcd585bb
SURFACEPRO9\mitsu <mitsuhiro.seino@gmail.com>
2025-08-05 22:23:59 +0900

    storybook

--- stories/CzLayoutContainer.stories.tsx
+++ stories/CzLayoutContainer.stories.tsx
@@ -9,7 +9,8 @@ import {
   SizeAdjust,
   SpacingProps,
   VAlign,
-} from '@cozka/react-layout/layouts/types';
+} from '@cozka/react-layout/layouts';
+import layout from '@cozka/react-layout/layouts/absolute';
 import { StyleProps } from '@cozka/react-style-props';
 import type { ArgTypes, Meta, StoryObj } from '@storybook/react';
 import CzLayoutContainer from '../src/CzLayoutContainer';
@@ -17,8 +18,8 @@ import _createContainerDecorator from './_createContainerDecorator';
 
 const LAYOUTS: LayoutType[] = [
   'absolute',
+  'balance',
   'brick',
-  'center',
   'fill',
   'matrix',
   'stack',
@@ -86,41 +87,103 @@ const ADJUST_ARG_TYPES: ArgTypes = {
 };
 
 const CHILD_SIZE_ARG_TYPES: ArgTypes = {
-  vSize: { type: 'number' },
-  hSize: { type: 'number' },
+  vSize: {
+    type: 'string',
+  },
+  hSize: {
+    type: 'string',
+  },
 };
 
 const SPACING_ARG_TYPES: ArgTypes = {
-  spacing: { type: 'number' },
-  hSpacing: { type: 'number' },
-  vSpacing: { type: 'number' },
+  spacing: {
+    type: 'string',
+  },
+  hSpacing: {
+    type: 'string',
+  },
+  vSpacing: {
+    type: 'string',
+  },
 };
 
 const CHILD_COUNT_ARG_TYPES: ArgTypes = {
-  hCount: { type: 'number' },
-  vCount: { type: 'number' },
+  hCount: {
+    type: 'string',
+  },
+  vCount: {
+    type: 'string',
+  },
 };
 
 const GRID_TEMPLATE_ARG_TYPES: ArgTypes = {
-  hTemplate: { type: 'string' },
-  vTemplate: { type: 'string' },
+  hTemplate: {
+    type: 'string',
+  },
+  vTemplate: {
+    type: 'string',
+  },
 };
 
 const CONTAINER_ARG_TYPES: ArgTypes = {
-  xHeight: { type: 'number' },
-  xWidth: { type: 'number' },
+  xHeight: { type: 'string' },
+  xWidth: { type: 'string' },
 };
 
-const ARG_TYPES: ArgTypes = {
-  ...LAYOUT_ARG_TYPES,
-  ...ORIENTATION_ARG_TYPES,
-  ...ALIGN_ARG_TYPES,
-  ...ADJUST_ARG_TYPES,
-  ...CHILD_SIZE_ARG_TYPES,
-  ...SPACING_ARG_TYPES,
-  ...CHILD_COUNT_ARG_TYPES,
-  ...GRID_TEMPLATE_ARG_TYPES,
-  ...CONTAINER_ARG_TYPES,
+const ARG_TYPES = {
+  all: {
+    ...LAYOUT_ARG_TYPES,
+    ...ORIENTATION_ARG_TYPES,
+    ...ALIGN_ARG_TYPES,
+    ...ADJUST_ARG_TYPES,
+    ...CHILD_SIZE_ARG_TYPES,
+    ...SPACING_ARG_TYPES,
+    ...CHILD_COUNT_ARG_TYPES,
+    ...CONTAINER_ARG_TYPES,
+    ...GRID_TEMPLATE_ARG_TYPES,
+  },
+  absolute: {
+    ...CHILD_SIZE_ARG_TYPES,
+    ...CONTAINER_ARG_TYPES,
+  },
+  balance: {
+    ...ORIENTATION_ARG_TYPES,
+    ...ADJUST_ARG_TYPES,
+    ...ALIGN_ARG_TYPES,
+    ...SPACING_ARG_TYPES,
+    ...CHILD_SIZE_ARG_TYPES,
+    ...CONTAINER_ARG_TYPES,
+  },
+  brick: {
+    ...ORIENTATION_ARG_TYPES,
+    ...ALIGN_ARG_TYPES,
+    ...SPACING_ARG_TYPES,
+    ...CHILD_COUNT_ARG_TYPES,
+    ...CHILD_SIZE_ARG_TYPES,
+    ...CONTAINER_ARG_TYPES,
+  },
+  fill: {
+    ...ORIENTATION_ARG_TYPES,
+    ...SPACING_ARG_TYPES,
+    ...CONTAINER_ARG_TYPES,
+  },
+  matrix: {
+    ...ORIENTATION_ARG_TYPES,
+    ...SPACING_ARG_TYPES,
+    ...GRID_TEMPLATE_ARG_TYPES,
+    ...CHILD_COUNT_ARG_TYPES,
+    ...CHILD_SIZE_ARG_TYPES,
+    ...CONTAINER_ARG_TYPES,
+  },
+  stack: {
+    ...ORIENTATION_ARG_TYPES,
+    ...ALIGN_ARG_TYPES,
+    ...ADJUST_ARG_TYPES,
+    ...CHILD_SIZE_ARG_TYPES,
+    ...SPACING_ARG_TYPES,
+    ...CHILD_COUNT_ARG_TYPES,
+    ...CONTAINER_ARG_TYPES,
+  },
 };
 
 const LAYOUT_PROPS = {
@@ -139,8 +202,8 @@ const ALIGN_PROPS: AlignProps = {
 };
 
 const CHILD_SIZE_PROPS: ChildSizeProps = {
-  vSize: 80,
-  hSize: 160,
+  vSize: '80',
+  hSize: '160',
 };
 
 const ORIENTATION_PROPS: OrientationProps = {
@@ -148,30 +211,100 @@ const ORIENTATION_PROPS: OrientationProps = {
 };
 
 const SPACING_PROPS: SpacingProps = {
-  spacing: 8,
+  spacing: '8',
   vSpacing: undefined,
   hSpacing: undefined,
 };
 
-const MATRIX_PROPS = {
-  hCount: 4,
-  vCount: 3,
+const CHILD_COUNT_PROPS: any = {
+  hCount: '4',
+  vCount: '3',
+};
+
+const GRID_TEMPLATE_PROPS = {
   hTemplate: undefined,
   vTemplate: undefined,
 };
 
 const CONTAINER_PROPS: StyleProps = {
-  xHeight: 400,
-  xWidth: 400,
-  xPadding: 0,
+  xHeight: '400',
+  xWidth: '400',
+  xPadding: '0',
 };
+
+const ARGS: Record<string, Record<string, any>> = {
+  all: {
+    ...LAYOUT_PROPS,
+    ...ORIENTATION_PROPS,
+    ...ALIGN_PROPS,
+    ...ADJUST_PROPS,
+    ...SPACING_PROPS,
+    ...GRID_TEMPLATE_PROPS,
+    ...CHILD_COUNT_PROPS,
+    ...CHILD_SIZE_PROPS,
+    ...CONTAINER_PROPS,
+  },
+  absolute: {
+    ...CHILD_SIZE_PROPS,
+    ...CONTAINER_PROPS,
+  },
+  balance: {
+    ...ORIENTATION_PROPS,
+    ...ALIGN_PROPS,
+    ...ADJUST_PROPS,
+    ...SPACING_PROPS,
+    ...CHILD_SIZE_PROPS,
+    ...CONTAINER_PROPS,
+  },
+  brick: {
+    ...ORIENTATION_PROPS,
+    ...ALIGN_PROPS,
+    ...SPACING_PROPS,
+    ...CHILD_SIZE_PROPS,
+    ...CONTAINER_PROPS,
+  },
+  fill: {
+    ...ORIENTATION_PROPS,
+    ...SPACING_PROPS,
+    ...CONTAINER_PROPS,
+  },
+  matrix: {
+    ...ORIENTATION_PROPS,
+    ...CHILD_COUNT_PROPS,
+    ...GRID_TEMPLATE_PROPS,
+    ...SPACING_PROPS,
+    ...CHILD_SIZE_PROPS,
+    ...CONTAINER_PROPS,
+  },
+  stack: {
+    ...ORIENTATION_PROPS,
+    ...ALIGN_PROPS,
+    ...ADJUST_PROPS,
+    ...SPACING_PROPS,
+    ...CHILD_SIZE_PROPS,
+    ...CONTAINER_PROPS,
+  },
+};
+
+const ENABLED_ARGS: Record<string, Record<string, any>> = {};
+for (const layout in ARGS) {
+  ENABLED_ARGS[layout] = {};
+  for (const arg in ARGS[layout]) {
+    ENABLED_ARGS[layout][arg] = true;
+  }
+}
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'CzLayoutContainer',
   component: CzLayoutContainer,
   tags: ['layout'],
-  decorators: _createContainerDecorator({ style: { backgroundColor: '#eee' } }),
+  decorators: _createContainerDecorator({
+    defaultProps: {
+      style: { backgroundColor: 'rgba(128, 128, 128, 0.1)' },
+    },
+    ENABLED_ARGS,
+  }),
 } satisfies Meta<typeof CzLayoutContainer>;
 
 export default meta;
@@ -179,18 +312,11 @@ type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Default: Story = {
-  argTypes: ARG_TYPES,
+  argTypes: ARG_TYPES.all,
   args: {
     layout: 'stack',
-    ...ORIENTATION_PROPS,
-    ...ALIGN_PROPS,
-    ...ADJUST_PROPS,
-    ...SPACING_PROPS,
-    ...MATRIX_PROPS,
-    ...LAYOUT_PROPS,
-    scroll: false,
-    ...CHILD_SIZE_PROPS,
-    ...CONTAINER_PROPS,
+    ...ARGS.all,
+    children: 12,
   },
   parameters: {
     randSize: true,
@@ -198,126 +324,79 @@ export const Default: Story = {
 };
 
 export const NoSize: Story = {
-  argTypes: ARG_TYPES,
+  argTypes: ARG_TYPES.all,
   args: {
     layout: 'stack',
-    ...ORIENTATION_PROPS,
-    ...ALIGN_PROPS,
-    ...ADJUST_PROPS,
-    ...SPACING_PROPS,
-    ...MATRIX_PROPS,
-    ...LAYOUT_PROPS,
-    scroll: false,
-    ...CONTAINER_PROPS,
+    ...ARGS.all,
+    children: 12,
   },
   parameters: {
     randSize: true,
     randPos: true,
   },
 };
-
 export const Absolute: Story = {
-  argTypes: {
-    ...LAYOUT_ARG_TYPES,
-    ...CHILD_SIZE_ARG_TYPES,
-    ...CONTAINER_ARG_TYPES,
-  },
+  argTypes: ARG_TYPES.absolute,
   args: {
-    layout: 'absolute',
-    ...LAYOUT_PROPS,
-    ...CHILD_SIZE_PROPS,
-    ...CONTAINER_PROPS,
+    ...ARGS.absolute,
+    children: 12,
+  },
+  parameters: {
+    defaultLayout: 'absolute',
+  },
+};
+
+export const Balance: Story = {
+  argTypes: ARG_TYPES.balance,
+  args: {
+    ...ARGS.balance,
+    children: 12,
+  },
+  parameters: {
+    defaultLayout: 'balance',
   },
 };
 
 export const Brick: Story = {
-  argTypes: {
-    ...ORIENTATION_ARG_TYPES,
-    ...ALIGN_ARG_TYPES,
-    ...SPACING_ARG_TYPES,
-    ...LAYOUT_ARG_TYPES,
-    ...CHILD_COUNT_ARG_TYPES,
-    ...CHILD_SIZE_ARG_TYPES,
-    ...CONTAINER_ARG_TYPES,
-  },
+  argTypes: ARG_TYPES.brick,
   args: {
-    layout: 'brick',
-    ...ORIENTATION_PROPS,
-    ...ALIGN_PROPS,
-    ...SPACING_PROPS,
-    ...LAYOUT_PROPS,
-    ...CHILD_SIZE_PROPS,
-    ...CONTAINER_PROPS,
+    ...ARGS.brick,
+    children: 12,
   },
-};
-
-export const Center: Story = {
-  argTypes: {
-    ...ORIENTATION_ARG_TYPES,
-    ...ADJUST_ARG_TYPES,
-    ...SPACING_ARG_TYPES,
-    ...LAYOUT_ARG_TYPES,
-    ...CHILD_SIZE_ARG_TYPES,
-    ...CONTAINER_ARG_TYPES,
-  },
-  args: {
-    layout: 'center',
-    ...ORIENTATION_PROPS,
-    ...ADJUST_PROPS,
-    ...SPACING_PROPS,
-    ...LAYOUT_PROPS,
-    ...CHILD_SIZE_PROPS,
-    ...CONTAINER_PROPS,
+  parameters: {
+    defaultLayout: 'brick',
   },
 };
 
 export const Fill: Story = {
-  argTypes: {
-    ...ORIENTATION_ARG_TYPES,
-    ...SPACING_ARG_TYPES,
-    ...LAYOUT_ARG_TYPES,
-    ...CONTAINER_ARG_TYPES,
-  },
+  argTypes: ARG_TYPES.fill,
   args: {
-    layout: 'fill',
-    ...ORIENTATION_PROPS,
-    ...SPACING_PROPS,
-    ...LAYOUT_PROPS,
-    ...CONTAINER_PROPS,
+    ...ARGS.fill,
+    children: 12,
+  },
+  parameters: {
+    defaultLayout: 'fill',
   },
 };
 
 export const Matrix: Story = {
-  argTypes: {
-    ...ORIENTATION_ARG_TYPES,
-    ...SPACING_ARG_TYPES,
-    ...GRID_TEMPLATE_ARG_TYPES,
-    ...LAYOUT_ARG_TYPES,
-    ...CHILD_COUNT_ARG_TYPES,
-    ...CHILD_SIZE_ARG_TYPES,
-    ...CONTAINER_ARG_TYPES,
-  },
+  argTypes: ARG_TYPES.matrix,
   args: {
-    layout: 'matrix',
-    ...ORIENTATION_PROPS,
-    ...SPACING_PROPS,
-    ...MATRIX_PROPS,
-    ...LAYOUT_PROPS,
-    ...CHILD_SIZE_PROPS,
-    ...CONTAINER_PROPS,
+    ...ARGS.matrix,
+    children: 12,
+  },
+  parameters: {
+    defaultLayout: 'matrix',
   },
 };
 
 export const Stack: Story = {
-  argTypes: ARG_TYPES,
+  argTypes: ARG_TYPES.stack,
   args: {
-    layout: 'stack',
-    ...ORIENTATION_PROPS,
-    ...ALIGN_PROPS,
-    ...ADJUST_PROPS,
-    ...SPACING_PROPS,
-    ...LAYOUT_PROPS,
-    ...CHILD_SIZE_PROPS,
-    ...CONTAINER_PROPS,
+    ...ARGS.stack,
+    children: 12,
+  },
+  parameters: {
+    defaultLayout: 'stack',
   },
 };
--- stories/_createContainerDecorator.tsx
+++ stories/_createContainerDecorator.tsx
@@ -7,24 +7,78 @@ function _random(scale: number) {
   return Math.floor(value * scale);
 }
 
+function _fromNumericStrings(obj: Record<string, any>) {
+  const result: Record<string, any> = {};
+  for (const key in obj) {
+    const value = obj[key];
+    result[key] = _fromNumericString(value);
+  }
+  return result;
+}
+
+function _fromNumericString(value: any) {
+  if (_isNumericString(value)) {
+    return Number(value);
+  }
+  return value || undefined;
+}
+
+function _isNumericString(value: any) {
+  return value != null && value !== '' && !isNaN(Number(value));
+}
+
 export default function _createContainerDecorator(
-  defaultProps?: Record<string, any>,
-  defaultChildProps?: Record<string, any>,
+  options: {
+    defaultProps?: Record<string, any>;
+    defaultChildProps?: Record<string, any>;
+    ENABLED_ARGS?: Record<string, Record<string, boolean>>;
+  } = {},
 ): Decorator {
-  return (Story, { args, parameters }: any) => {
-    const { children = 12, randSize, randPos } = parameters;
-    const colors = chroma.scale(['d9ed92', '184e77']).colors(children);
-    const props = {
+  const { defaultProps = {}, defaultChildProps = {} } = options;
+
+  return (Story, configs: any) => {
+    const { args, parameters } = configs;
+    const { randSize, randPos, defaultLayout } = parameters;
+    const { layout = defaultLayout, children = 12, ...rest } = args;
+    const colors = chroma
+      .scale(['d9ed92', '184e77'])
+      .colors(children as number);
+    const {
+      hSize,
+      vSize,
+      spacing,
+      hSpacing,
+      vSpacing,
+      hCount,
+      vCount,
+      xHeight,
+      xWidth,
+      xPadding,
+      ...restProps
+    } = {
       ...defaultProps,
-      ...args,
+      ...rest,
     };
 
     return (
       <Story
         args={{
-          ...props,
+          layout,
+          ...restProps,
+          ..._fromNumericStrings({
+            hSize,
+            vSize,
+            spacing,
+            hSpacing,
+            vSpacing,
+            hCount,
+            vCount,
+            xHeight,
+            xWidth,
+            xPadding,
+          }),
           children: colors.map((color, index) => {
-            const props = {
+            const childProps = {
               xHeight: 80,
               xWidth: 160,
               style: {
@@ -34,12 +88,12 @@ export default function _createContainerDecorator(
               },
             };
             if (randSize) {
-              props.xHeight = _random(100);
-              props.xWidth = _random(200);
+              childProps.xHeight = _random(100);
+              childProps.xWidth = _random(200);
             }
             if (randPos) {
-              props.style.top = _random(args.xHeight ?? 40);
-              props.style.left = _random(args.xWidth ?? 40);
+              childProps.style.top = _random(args.xHeight ?? 40);
+              childProps.style.left = _random(args.xWidth ?? 40);
             }
 
             return (
@@ -48,7 +102,7 @@ export default function _createContainerDecorator(
                 xDisplay="flex"
                 xJustifyContent="center"
                 xAlignItems="center"
-                {...props}
+                {...childProps}
                 {...defaultChildProps}
               >
                 {index + 1}
